92a730331e2e90ead85af508f3e3371544a40593
Joctan Silva <joctannsilva@gmail.com>
2024-05-09 00:10:17 -0300

    fix gallery name

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,7 +5,7 @@ export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b  p-4  text-xl font-semibold">
       <div>
-        <a href="/">Jocs Gallery</a>
+        <a href="/">Joctan Gallery</a>
       </div>
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
